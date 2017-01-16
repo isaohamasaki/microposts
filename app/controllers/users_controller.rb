df32dad1ed965a349e06f31bc1278ce046ffe4ca
isaohamasaki <masarulana1121@s3.dion.ne.jp>
2017-01-16 12:20:12 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show # 追加
-   @user = User.find(params[:id])
   end
   
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,11 +18,31 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "UpdateProfile!"
+      redirect_to @user # ここを修正
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:location)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    redirect_to root_path if @user != current_user
   end
 end
